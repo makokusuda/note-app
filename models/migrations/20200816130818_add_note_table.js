f36ec3983af57b13939ff3c9f17491a88173fa3a
Mako Kusuda <mako.kusuda@gmail.com>
2020-08-29 16:41:13 +0900

    Fix migration to update timestamp

--- models/migrations/20200816130818_add_note_table.js
+++ models/migrations/20200816130818_add_note_table.js
@@ -1,12 +1,17 @@
-exports.up = function (knex, Promise) {
-  return knex.schema.createTable("notes", (t) => {
+exports.up = async function (knex) {
+  await knex.schema.createTable("notes", (t) => {
     t.increments().index(); // id
     t.string("body");
-    t.timestamp("created_at").defaultTo(knex.fn.now());
-    t.timestamp("updated_at").defaultTo(knex.fn.now());
+    t.timestamps(false, true);
   });
+  await knex.raw(`CREATE TRIGGER update_timestamp
+  BEFORE UPDATE
+  ON notes
+  FOR EACH ROW
+  EXECUTE PROCEDURE update_timestamp();
+`);
 };
 
-exports.down = function (knex, Promise) {
+exports.down = function (knex) {
   return knex.schema.dropTable("notes");
 };
